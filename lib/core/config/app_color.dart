f905ce00517bf0ca42fd1bb1a8e51e1145bca0ee
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-06 15:20:09 +0200

    feat: add core application color definitions

---
+++ lib/core/config/app_color.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color primary100 = Color(0xFF8E6CEF);
+  static const Color primary50 = Color(0xFFEFEAF5);
+  static const Color bgLight1 = Color(0xFFFFFFFF);
+  static const Color bgLight2 = Color(0xFFF4F4F4);
+  static const Color bgDark1 = Color(0xFF1D182A);
+  static const Color bgDark2 = Color(0xFF342F3F);
+}
