dea3d0afa00d2bd2501aeda4f0d35cf02334f7dc
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-07 07:43:48 +0200

    feat: add initial light and dark theme configurations

---
+++ lib/core/config/app_theme.dart
@@ -0,0 +1,37 @@
+import 'package:e_commerce/core/config/app_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: AppColor.bgLight1,
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColor.bgLight1,
+
+      centerTitle: true,
+      shadowColor: Colors.transparent,
+      iconTheme: const IconThemeData(color: Colors.black),
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 16.sp,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: AppColor.bgLight1,
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColor.bgDark1,
+      centerTitle: true,
+      shadowColor: Colors.transparent,
+      iconTheme: const IconThemeData(color: Colors.white),
+      titleTextStyle: TextStyle(
+        color: Colors.white,
+        fontSize: 16.sp,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
