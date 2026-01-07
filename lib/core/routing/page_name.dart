ff1951595be70ea31997a434b63c84e692ac16a9
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-07 16:01:00 +0200

    feat: Creating pageName class to use it in go router instead hard codeed and apply this for splash

---
+++ lib/core/routing/page_name.dart
@@ -0,0 +1,5 @@
+class PageName {
+  static const String splash = '/';
+  static const String emailSignIn = '/email-sign-in';
+  static const String passwordSignIn = '/password-sign-in';
+}
--- lib/feature/splash/presentation/routing/splash_route.dart
+++ lib/feature/splash/presentation/routing/splash_route.dart
@@ -1,7 +1,8 @@
 import 'package:e_commerce/feature/splash/presentation/pages/splash_page.dart';
 import 'package:go_router/go_router.dart';
+import 'package:e_commerce/core/routing/page_name.dart';
 
 final splashRoute = GoRoute(
-  path: '/',
+  path: PageName.splash,
   builder: (context, state) => const SplashPage(),
 );
