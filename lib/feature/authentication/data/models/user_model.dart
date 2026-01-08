a738b7dd6032241276eae656d5743108a6c6fe80
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-08 15:40:49 +0200

    create user model

---
+++ lib/feature/authentication/data/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final String email;
+  final String password;
+  final String firstName;
+  final String lastName;
+
+  UserModel({
+    required this.email,
+    required this.password,
+    required this.firstName,
+    required this.lastName,
+  });
+}
