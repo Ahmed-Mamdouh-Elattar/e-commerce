2bef4f9a7ff1bc752d0a15237f03167f1f7a25d6
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-09 15:12:19 +0200

    handle the link that coming form email confirmation to make deep link work correctly

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -2,4 +2,15 @@ import 'package:e_commerce/feature/authentication/presentation/routing/sign_in_r
 import 'package:e_commerce/feature/splash/presentation/routing/splash_route.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(routes: [splashRoute, signInRoute]);
+final appRouter = GoRouter(
+  redirect: (context, state) {
+    final location = state.uri.toString();
+
+    if (location.startsWith('e-commerce://')) {
+      return location.replaceFirst('e-commerce://', '/');
+    }
+
+    return null;
+  },
+  routes: [splashRoute, signInRoute],
+);
