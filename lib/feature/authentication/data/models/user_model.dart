ec400bfc7334f27c3f4e47e56ab3016192796523
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-10 14:02:09 +0200

    make firstname and lastname nullable in userModel

--- lib/feature/authentication/data/models/user_model.dart
+++ lib/feature/authentication/data/models/user_model.dart
@@ -1,13 +1,13 @@
 class UserModel {
   final String email;
   final String password;
-  final String firstName;
-  final String lastName;
+  final String? firstName;
+  final String? lastName;
 
   UserModel({
     required this.email,
     required this.password,
-    required this.firstName,
-    required this.lastName,
+    this.firstName,
+    this.lastName,
   });
 }
