e1d6fb162d7213f510e4ba5d6d1195100b78957b
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-11 13:23:35 +0200

    create authStateChangeProvider to handle ui states

---
+++ lib/feature/authentication/presentation/provider/auth_state_change_provider/auth_state_change_provider.dart
@@ -0,0 +1,10 @@
+import 'package:e_commerce/feature/authentication/domain/usecases/auth_state_change_usecase.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final authStateChangeProvider = StreamProvider<AuthState>((ref) async* {
+  final authStateChangeUseCase = ref.read(authStateChangeUseCaseProvider);
+  await for (final authState in authStateChangeUseCase.call()) {
+    yield authState;
+  }
+});
