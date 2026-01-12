c438b06721e92d94c186143b0762b2316d2658e8
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-12 23:58:17 +0200

    handle navigation in splash page to navigate to home or sign in listen to auth state in redirect to return user to sign in page when sign out

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,19 +1,45 @@
+import 'package:e_commerce/core/providers/auth_state_change_provider/auth_state_change_provider.dart';
+import 'package:e_commerce/core/routing/page_name.dart';
 import 'package:e_commerce/feature/authentication/presentation/routing/sign_in_route.dart';
 import 'package:e_commerce/feature/splash/presentation/routing/splash_route.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
+  final routerNotifier = ref.watch(routerNotifierProvider);
   return GoRouter(
+    refreshListenable: routerNotifier,
     redirect: (context, state) {
+      final authStateChange = ref.read(authStateChangeProvider);
       final location = state.uri.toString();
 
       if (location.startsWith('e-commerce://')) {
         return location.replaceFirst('e-commerce://', '/');
+      }
+      if (authStateChange.value?.event == AuthChangeEvent.signedOut) {
+        return PageName.emailSignIn;
       }
 
       return null;
     },
     routes: [splashRoute, signInRoute],
   );
+});
+
+class RouterNotifier extends ChangeNotifier {
+  final Ref _ref;
+
+  RouterNotifier(this._ref) {
+    _ref.listen(authStateChangeProvider, (previous, next) {
+      next.whenData((data) {
+        notifyListeners();
+      });
+    });
+  }
+}
+
+final routerNotifierProvider = Provider<RouterNotifier>((ref) {
+  return RouterNotifier(ref);
 });
--- lib/feature/splash/presentation/widgets/splash_page_body.dart
+++ lib/feature/splash/presentation/widgets/splash_page_body.dart
@@ -1,30 +1,41 @@
-import 'dart:async';
-
 import 'package:e_commerce/core/helper/assets.gen.dart';
+import 'package:e_commerce/core/providers/auth_state_change_provider/auth_state_change_provider.dart';
 import 'package:e_commerce/core/routing/page_name.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
-class SplashPageBody extends HookWidget {
+class SplashPageBody extends ConsumerWidget {
   const SplashPageBody({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    useEffect(() {
-      Timer timer = Timer(const Duration(seconds: 2), () {
-        context.go(PageName.emailSignIn);
+  Widget build(BuildContext context, WidgetRef ref) {
+    bool isSplashCompleted = false;
+    ref.listen(authStateChangeProvider, (previous, next) {
+      next.whenData((data) {
+        if (data.session != null && isSplashCompleted) {
+          context.go(PageName.home);
+        } else if (data.session == null && isSplashCompleted) {
+          context.go(PageName.emailSignIn);
+        }
       });
-      return () => timer.cancel();
     });
     return Center(
       child: SizedBox(
         width: 200.w,
-        child: Image.asset(
-          Assets.images.splashLogo.path,
-        ).animate().scale(duration: 500.ms).fade(duration: 500.ms),
+        child: Image.asset(Assets.images.splashLogo.path)
+            .animate(
+              onComplete: (controller) {
+                Future.delayed(const Duration(seconds: 1), () {
+                  isSplashCompleted = true;
+                  ref.invalidate(authStateChangeProvider);
+                });
+              },
+            )
+            .scale(duration: 500.ms)
+            .fade(duration: 500.ms),
       ),
     );
   }
