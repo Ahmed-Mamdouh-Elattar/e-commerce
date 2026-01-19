964a2d4ff4725b1c0eeb89684869570d862567dc
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-19 23:58:58 +0200

    feat: implement category listing for the home feature with remote data fetching and display widgets.

--- lib/feature/home/presentation/widgets/category_list_tile_item.dart
+++ lib/feature/home/presentation/widgets/category_list_tile_item.dart
@@ -1,15 +1,16 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:e_commerce/core/config/app_color.dart';
 import 'package:e_commerce/core/config/app_text_style.dart';
 import 'package:e_commerce/core/extensions/theme_extension.dart';
 import 'package:e_commerce/core/routing/page_name.dart';
-import 'package:e_commerce/feature/home/presentation/helper/category_ui_model.dart';
+import 'package:e_commerce/feature/home/domain/entities/category_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class CategoryListTileItem extends StatelessWidget {
   const CategoryListTileItem({required this.category, super.key});
 
-  final CategoryUiModel category;
+  final CategoryEntity category;
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +20,8 @@ class CategoryListTileItem extends StatelessWidget {
       tileColor: context.isDarkMode ? AppColor.bgDark2 : AppColor.bgLight2,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       contentPadding: const EdgeInsets.all(12),
-      leading: Image.asset(category.image),
-      title: Text(category.title, style: AppTextStyle.medium16),
+      leading: CachedNetworkImage(imageUrl: category.image),
+      title: Text(category.name, style: AppTextStyle.medium16),
     );
   }
 }
--- lib/feature/home/presentation/widgets/category_list_view.dart
+++ lib/feature/home/presentation/widgets/category_list_view.dart
@@ -1,37 +1,47 @@
-import 'package:e_commerce/core/helper/assets.gen.dart';
-import 'package:e_commerce/feature/home/presentation/helper/category_ui_model.dart';
+import 'package:e_commerce/feature/home/domain/entities/category_entity.dart';
+import 'package:e_commerce/feature/home/presentation/riverpod/get_categories_provider/get_categories_provider.dart';
 import 'package:e_commerce/feature/home/presentation/widgets/category_list_tile_item.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
-class CategoryListView extends StatefulWidget {
+class CategoryListView extends ConsumerWidget {
   const CategoryListView({super.key});
 
   @override
-  State<CategoryListView> createState() => _CategoryListViewState();
-}
-
-class _CategoryListViewState extends State<CategoryListView> {
-  final List<CategoryUiModel> categories = [
-    CategoryUiModel(image: Assets.images.dummyHoodie.path, title: "Hoodies"),
-    CategoryUiModel(image: Assets.images.dummyShort.path, title: "Short"),
-    CategoryUiModel(image: Assets.images.dummyShoes.path, title: "Shoes"),
-    CategoryUiModel(image: Assets.images.dummyBag.path, title: "Bag"),
-    CategoryUiModel(
-      image: Assets.images.dummyAcccessories.path,
-      title: "Acccessories",
-    ),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.separated(
-      separatorBuilder: (context, index) {
-        return const SizedBox(height: 8);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final categories = ref.watch(getCategoriesProvider);
+    return categories.when(
+      data: (data) {
+        return ListView.separated(
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 8);
+          },
+          shrinkWrap: true,
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            return CategoryListTileItem(category: data[index]);
+          },
+        );
       },
-      shrinkWrap: true,
-      itemCount: categories.length,
-      itemBuilder: (context, index) {
-        return CategoryListTileItem(category: categories[index]);
+      loading: () {
+        return Skeletonizer(
+          child: ListView.separated(
+            separatorBuilder: (context, index) {
+              return const SizedBox(height: 8);
+            },
+            shrinkWrap: true,
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return const CategoryListTileItem(
+                category: CategoryEntity(id: "", name: "", image: ""),
+              );
+            },
+          ),
+        );
+      },
+      error: (error, stackTrace) {
+        return Center(child: Text(error.toString()));
       },
     );
   }
