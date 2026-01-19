b32b729e5719d6acc096450ae9ce11c6fec80738
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-19 23:22:12 +0200

    create getNewInProducts in HomeRemoteDataSource and implement it in HomeRemoteDataSourceImpl

--- lib/feature/home/data/datasources/home_remote_data_source.dart
+++ lib/feature/home/data/datasources/home_remote_data_source.dart
@@ -8,6 +8,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 abstract class HomeRemoteDataSource {
   Future<List<CategoryModel>> getCategories();
   Future<List<ProductModel>> getTopSellingProducts();
+  Future<List<ProductModel>> getNewInProducts();
 }
 
 class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
@@ -31,6 +32,22 @@ class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
           .from('products')
           .select("id,title,price,images")
           .eq("is_top_selling", true);
+      log(data.toString());
+      return data.map((e) => ProductModel.fromJson(e)).toList();
+    } on PostgrestException catch (e) {
+      throw Failure(message: e.message);
+    } catch (e) {
+      throw Failure(message: e.toString());
+    }
+  }
+
+  @override
+  Future<List<ProductModel>> getNewInProducts() async {
+    try {
+      final data = await Supabase.instance.client
+          .from('products')
+          .select("id,title,price,images")
+          .eq("is_new_in", true);
       log(data.toString());
       return data.map((e) => ProductModel.fromJson(e)).toList();
     } on PostgrestException catch (e) {
