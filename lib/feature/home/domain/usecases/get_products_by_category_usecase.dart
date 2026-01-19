6a348ac2ce04f4cf448e65a73a8d8dccfad4f3e9
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-20 01:41:33 +0200

    create getProductsByCategoryUseCaseProvider

--- lib/feature/home/domain/usecases/get_products_by_category_usecase.dart
+++ lib/feature/home/domain/usecases/get_products_by_category_usecase.dart
@@ -1,5 +1,7 @@
+import 'package:e_commerce/feature/home/data/repositories/home_repo_impl_provider.dart';
 import 'package:e_commerce/feature/home/domain/entities/product_entity.dart';
 import 'package:e_commerce/feature/home/domain/repositories/home_repo.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class GetProductsByCategoryUseCase {
   final HomeRepo _homeRepo;
@@ -10,3 +12,10 @@ class GetProductsByCategoryUseCase {
     return await _homeRepo.getProductsByCategory(categoryId);
   }
 }
+
+final getProductsByCategoryUseCaseProvider =
+    Provider<GetProductsByCategoryUseCase>((ref) {
+      return GetProductsByCategoryUseCase(
+        homeRepo: ref.read(homeRepoImplProvider),
+      );
+    });
