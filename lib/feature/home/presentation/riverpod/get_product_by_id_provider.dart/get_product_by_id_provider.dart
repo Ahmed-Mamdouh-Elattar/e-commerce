34db7d61e87382821476bb504534af0454959f7b
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-20 05:45:46 +0200

    create getProductByIdProvider to handle states in Ui

---
+++ lib/feature/home/presentation/riverpod/get_product_by_id_provider.dart/get_product_by_id_provider.dart
@@ -0,0 +1,9 @@
+import 'package:e_commerce/feature/home/domain/entities/product_entity.dart';
+import 'package:e_commerce/feature/home/domain/usecases/get_product_by_id_usecase.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final getProductByIdProvider = FutureProvider.family
+    .autoDispose<ProductEntity, String>((ref, productId) async {
+      final getProductByIdUsecase = ref.read(getProductByIdUsecaseProvider);
+      return getProductByIdUsecase.getProductById(productId);
+    });
