5da2b09bb547ae4298592e156ee4ad65b6e6455a
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-20 05:45:14 +0200

    create GetProductByIdUsecase create getProductByIdUsecaseProvider

---
+++ lib/feature/home/domain/usecases/get_product_by_id_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:e_commerce/feature/home/data/repositories/home_repo_impl_provider.dart';
+import 'package:e_commerce/feature/home/domain/entities/product_entity.dart';
+import 'package:e_commerce/feature/home/domain/repositories/home_repo.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class GetProductByIdUsecase {
+  final HomeRepo _homeRepo;
+  GetProductByIdUsecase({required HomeRepo homeRepo}) : _homeRepo = homeRepo;
+  Future<ProductEntity> getProductById(String productId) async {
+    return await _homeRepo.getProductById(productId);
+  }
+}
+
+final getProductByIdUsecaseProvider = Provider<GetProductByIdUsecase>((ref) {
+  return GetProductByIdUsecase(homeRepo: ref.read(homeRepoImplProvider));
+});
