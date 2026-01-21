e4013418a7ad0ed1da721ef7f0ac73abd43a9ea2
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-21 06:33:25 +0200

    Refactor favorite buttons to use Riverpod for wishlist state

--- lib/feature/wishlist/presentation/widgets/favorite_icon_button.dart
+++ lib/feature/wishlist/presentation/widgets/favorite_icon_button.dart
@@ -1,21 +1,50 @@
 import 'package:e_commerce/core/helper/assets.gen.dart';
+import 'package:e_commerce/feature/wishlist/presentation/provider/add_or_remove_product_from_wishlist_provider/add_or_remove_product_from_wishlist_provider.dart';
+import 'package:e_commerce/feature/wishlist/presentation/provider/get_wishlist_product_id_provider/get_wishlist_product_ids_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:vector_graphics/vector_graphics_compat.dart';
 
-class FavoriteIconButton extends StatelessWidget {
-  const FavoriteIconButton({this.isFavorite = false, super.key});
-  final bool isFavorite;
+class FavoriteIconButton extends HookConsumerWidget {
+  const FavoriteIconButton({required this.productId, super.key});
+  final String productId;
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isActuallyFavorite = ref.watch(
+      getWishlistProductIdsProvider.select(
+        (value) => value.value?.contains(productId) ?? false,
+      ),
+    );
+    final optimisticIsFavorite = useState(isActuallyFavorite);
+    useEffect(() {
+      optimisticIsFavorite.value = isActuallyFavorite;
+      return null;
+    }, [isActuallyFavorite]);
+    ref.listen(addOrRemoveProductFromWishlistProvider, (previous, next) {
+      next.maybeWhen(
+        error: (error, stackTrace) {
+          optimisticIsFavorite.value = isActuallyFavorite;
+        },
+        orElse: () {},
+      );
+    });
     return IconButton(
       padding: EdgeInsets.zero,
-      onPressed: () {},
+      onPressed: () {
+        ref
+            .read(addOrRemoveProductFromWishlistProvider.notifier)
+            .toggleProductFromWishlist(optimisticIsFavorite.value, productId);
+        optimisticIsFavorite.value = !optimisticIsFavorite.value;
+      },
       icon: VectorGraphic(
-        height: 24.h,
-        width: 24.w,
+        height: optimisticIsFavorite.value ? 30.h : 24.h,
+        width: optimisticIsFavorite.value ? 30.w : 24.w,
         loader: AssetBytesLoader(
-          isFavorite ? Assets.svg.heart : Assets.svg.balckFavorite,
+          optimisticIsFavorite.value
+              ? Assets.svg.heart
+              : Assets.svg.balckFavorite,
         ),
       ),
     );
--- lib/feature/wishlist/presentation/widgets/favorite_icon_button_with_circle_shape.dart
+++ lib/feature/wishlist/presentation/widgets/favorite_icon_button_with_circle_shape.dart
@@ -1,14 +1,42 @@
 import 'package:e_commerce/core/config/app_color.dart';
 import 'package:e_commerce/core/extensions/theme_extension.dart';
 import 'package:e_commerce/core/helper/assets.gen.dart';
+import 'package:e_commerce/feature/wishlist/presentation/provider/add_or_remove_product_from_wishlist_provider/add_or_remove_product_from_wishlist_provider.dart';
+import 'package:e_commerce/feature/wishlist/presentation/provider/get_wishlist_product_id_provider/get_wishlist_product_ids_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:vector_graphics/vector_graphics_compat.dart';
 
-class FavoriteIconButtonWithCircleShape extends StatelessWidget {
-  const FavoriteIconButtonWithCircleShape({this.isFavorite = false, super.key});
-  final bool isFavorite;
+class FavoriteIconButtonWithCircleShape extends HookConsumerWidget {
+  const FavoriteIconButtonWithCircleShape({required this.productId, super.key});
+
+  final String productId;
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isActuallyFavorite = ref.watch(
+      getWishlistProductIdsProvider.select(
+        (value) => value.value?.contains(productId) ?? false,
+      ),
+    );
+
+    final optimisticIsFavorite = useState(isActuallyFavorite);
+
+    useEffect(() {
+      optimisticIsFavorite.value = isActuallyFavorite;
+      return null;
+    }, [isActuallyFavorite]);
+
+    ref.listen(addOrRemoveProductFromWishlistProvider, (previous, next) {
+      next.maybeWhen(
+        error: (error, stackTrace) {
+          optimisticIsFavorite.value = isActuallyFavorite;
+        },
+        orElse: () {},
+      );
+    });
+
     return IconButton(
       icon: Container(
         height: 40,
@@ -19,15 +47,22 @@ class FavoriteIconButtonWithCircleShape extends StatelessWidget {
         ),
         child: Center(
           child: VectorGraphic(
-            height: 16,
-            width: 16,
+            height: optimisticIsFavorite.value ? 20 : 16,
+            width: optimisticIsFavorite.value ? 20 : 16,
             loader: AssetBytesLoader(
-              isFavorite ? Assets.svg.heart : Assets.svg.balckFavorite,
+              optimisticIsFavorite.value
+                  ? Assets.svg.heart
+                  : Assets.svg.balckFavorite,
             ),
           ),
         ),
       ),
-      onPressed: () {},
+      onPressed: () {
+        ref
+            .read(addOrRemoveProductFromWishlistProvider.notifier)
+            .toggleProductFromWishlist(optimisticIsFavorite.value, productId);
+        optimisticIsFavorite.value = !optimisticIsFavorite.value;
+      },
     );
   }
 }
