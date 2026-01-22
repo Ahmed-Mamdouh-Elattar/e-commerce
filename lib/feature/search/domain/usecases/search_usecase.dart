0d708e4e9f0d623ce44076e042a60225f04495c1
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-22 03:21:18 +0200

    feat(search): add SearchUsecase with provider and repository integration

--- lib/feature/search/domain/usecases/search_usecase.dart
+++ lib/feature/search/domain/usecases/search_usecase.dart
@@ -1,1 +1,16 @@
+import 'package:e_commerce/core/entities/product_entity.dart';
+import 'package:e_commerce/feature/search/data/repositories/search_repo_impl_provider.dart';
+import 'package:e_commerce/feature/search/domain/repositories/search_repo.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+class SearchUsecase {
+  final SearchRepo _searchRepo;
+  SearchUsecase({required SearchRepo searchRepo}) : _searchRepo = searchRepo;
+  Future<List<ProductEntity>> call(String query) async {
+    return _searchRepo.searchProducts(query);
+  }
+}
+
+final searchUsecase = Provider<SearchUsecase>((ref) {
+  return SearchUsecase(searchRepo: ref.read(searchRepoImpl));
+});
