9e287c1f2a017a0ee3df06f9b0ddb266a174aaec
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-26 05:46:16 +0200

    feat(profile): add updateUserData with connectivity check

--- lib/feature/profile/data/repositories/profile_repo_impl.dart
+++ lib/feature/profile/data/repositories/profile_repo_impl.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:e_commerce/core/error/failure.dart';
 import 'package:e_commerce/feature/profile/data/datasources/profile_remote_data_source.dart';
 import 'package:e_commerce/feature/profile/domain/entities/user_entity.dart';
@@ -5,13 +8,27 @@ import 'package:e_commerce/feature/profile/domain/repositories/profile_repo.dart
 
 class ProfileRepoImpl implements ProfileRepo {
   final ProfileRemoteDataSource _remoteDataSource;
+  final Connectivity _connectivity;
 
-  ProfileRepoImpl(this._remoteDataSource);
+  ProfileRepoImpl(this._remoteDataSource, this._connectivity);
   @override
   UserEntity getUserData() {
     try {
       final user = _remoteDataSource.getUserData();
       return user.toUserEntity();
+    } catch (e) {
+      throw Failure(message: e.toString());
+    }
+  }
+
+  @override
+  Future<void> updateUserData(String name, Uint8List? image) async {
+    final connectivityResult = await _connectivity.checkConnectivity();
+    if (connectivityResult.contains(ConnectivityResult.none)) {
+      throw Failure(message: 'No internet connection');
+    }
+    try {
+      await _remoteDataSource.updateUserData(name, image);
     } catch (e) {
       throw Failure(message: e.toString());
     }
--- lib/feature/profile/domain/repositories/profile_repo.dart
+++ lib/feature/profile/domain/repositories/profile_repo.dart
@@ -1,5 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:e_commerce/feature/profile/domain/entities/user_entity.dart';
 
 abstract class ProfileRepo {
   UserEntity getUserData();
+  Future<void> updateUserData(String name, Uint8List? image);
 }
