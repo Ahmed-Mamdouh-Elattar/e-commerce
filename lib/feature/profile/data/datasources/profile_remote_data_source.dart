8997803129ef2645efcbe622913c4033c820e934
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-26 05:39:44 +0200

    feat(profile): add updateUserData method with image upload support

--- lib/feature/profile/data/datasources/profile_remote_data_source.dart
+++ lib/feature/profile/data/datasources/profile_remote_data_source.dart
@@ -1,9 +1,12 @@
+import 'dart:typed_data';
+
 import 'package:e_commerce/core/error/failure.dart';
 import 'package:e_commerce/feature/profile/data/models/user_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class ProfileRemoteDataSource {
   UserModel getUserData();
+  Future<void> updateUserData(String name, Uint8List? image);
 }
 
 class ProfileRemoteDataSourceImpl implements ProfileRemoteDataSource {
@@ -16,10 +19,60 @@ class ProfileRemoteDataSourceImpl implements ProfileRemoteDataSource {
 
     return UserModel(
       id: user.id,
-      name:
-          "${user.userMetadata?['first_name']} ${user.userMetadata?['last_name']}",
+      name: user.userMetadata?['name'],
       email: user.email,
       image: user.userMetadata?['avatar_url'],
     );
+  }
+
+  @override
+  Future<void> updateUserData(String name, Uint8List? image) async {
+    String? imageUrl;
+    final user = Supabase.instance.client.auth.currentUser;
+    if (user == null) {
+      throw Failure(message: 'User not found');
+    }
+    if (image != null) {
+      imageUrl = await uploadImageToSupabaseStorage(user, image, imageUrl);
+    }
+
+    await Supabase.instance.client.auth.updateUser(
+      UserAttributes(
+        data: {if (image != null) 'avatar_url': imageUrl, 'name': name},
+      ),
+    );
+  }
+
+  Future<String?> uploadImageToSupabaseStorage(
+    User user,
+    Uint8List image,
+    String? imageUrl,
+  ) async {
+    final path =
+        '${user.id}/avatar_${DateTime.now().millisecondsSinceEpoch}.jpg';
+
+    final List<FileObject> existingFiles = await Supabase
+        .instance
+        .client
+        .storage
+        .from('avatars')
+        .list(path: user.id);
+
+    if (existingFiles.isNotEmpty) {
+      final List<String> filesToDelete = existingFiles
+          .map((f) => '${user.id}/${f.name}')
+          .toList();
+      await Supabase.instance.client.storage
+          .from('avatars')
+          .remove(filesToDelete);
+    }
+
+    await Supabase.instance.client.storage
+        .from('avatars')
+        .uploadBinary(path, image);
+    imageUrl = Supabase.instance.client.storage
+        .from('avatars')
+        .getPublicUrl(path);
+    return imageUrl;
   }
 }
