c4cc454e6e3fe55805870ea017002ca5abc662d4
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-28 10:53:28 +0200

    Refines cart product list management
    Makes the cart products provider `autoDispose` to leverage Riverpod's automatic state management capabilities. This change removes the need for manual invalidation after adding items to the cart, simplifying state synchronization and improving resource management.

--- lib/feature/cart/presentation/providers/get_cart_products_provider/get_cart_products_provider.dart
+++ lib/feature/cart/presentation/providers/get_cart_products_provider/get_cart_products_provider.dart
@@ -2,7 +2,9 @@ import 'package:e_commerce/feature/cart/domain/entities/cart_entitity.dart';
 import 'package:e_commerce/feature/cart/domain/usecases/get_cart_product_usecase.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final getCartProductsProvider = FutureProvider<List<CartEntity>>((ref) async {
+final getCartProductsProvider = FutureProvider.autoDispose<List<CartEntity>>((
+  ref,
+) async {
   final getCartProductUseCase = ref.read(getCartProductUseCaseProvider);
   return await getCartProductUseCase.call();
 });
