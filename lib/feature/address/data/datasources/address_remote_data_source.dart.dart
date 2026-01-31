a893f2444dbf728b31727d29e2d7a51091d8ae49
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-31 16:00:48 +0200

    Adds remote user address retrieval
    Introduces the ability to fetch a list of addresses associated with the currently authenticated user from the remote data source. This involves querying the 'addresses' table in Supabase, mapping the results to `UserAddressModel` objects, and includes appropriate error handling. This enables users to view and manage their saved addresses.

--- lib/feature/address/data/datasources/address_remote_data_source.dart.dart
+++ lib/feature/address/data/datasources/address_remote_data_source.dart.dart
@@ -4,6 +4,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class AddressRemoteDataSource {
   Future<void> addAddress(UserAddressModel address);
+  Future<List<UserAddressModel>> getAddresses();
 }
 
 class AddressRemoteDataSourceImpl implements AddressRemoteDataSource {
@@ -17,6 +18,25 @@ class AddressRemoteDataSourceImpl implements AddressRemoteDataSource {
       await Supabase.instance.client
           .from('addresses')
           .insert(address.copyWith(userId: userId).toMap());
+    } on PostgrestException catch (e) {
+      throw Failure(message: e.message);
+    } catch (e) {
+      throw Failure(message: e.toString());
+    }
+  }
+
+  @override
+  Future<List<UserAddressModel>> getAddresses() async {
+    try {
+      final userId = Supabase.instance.client.auth.currentUser?.id;
+      if (userId == null) {
+        throw Failure(message: 'User not authenticated');
+      }
+      final response = await Supabase.instance.client
+          .from('addresses')
+          .select('*')
+          .eq('user_id', userId);
+      return response.map((e) => UserAddressModel.fromMap(e)).toList();
     } on PostgrestException catch (e) {
       throw Failure(message: e.message);
     } catch (e) {
