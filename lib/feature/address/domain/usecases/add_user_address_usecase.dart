b472bc0d28d80bbc81cf4f3ec656ede69175390a
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-31 11:31:29 +0200

    Adds use case for adding user address
    Creates a dedicated `AddUserAddressUsecase` to encapsulate the logic for adding a new user address. This use case interacts with the `AddressRepo` and is exposed via a Riverpod provider for easy dependency injection.
    
    Deletes an empty and unused `address_usecase.dart` file.

---
+++ lib/feature/address/domain/usecases/add_user_address_usecase.dart
@@ -0,0 +1,18 @@
+import 'package:e_commerce/feature/address/data/repositories/address_repo_impl_provider.dart';
+import 'package:e_commerce/feature/address/domain/entities/user_address_entity.dart';
+import 'package:e_commerce/feature/address/domain/repositories/address_repo.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class AddUserAddressUsecase {
+  final AddressRepo _addressRepo;
+  AddUserAddressUsecase({required AddressRepo addressRepo})
+    : _addressRepo = addressRepo;
+
+  Future<void> call(UserAddressEntity address) async {
+    return _addressRepo.addAddress(address);
+  }
+}
+
+final addUserAddressUsecaseProvider = Provider<AddUserAddressUsecase>((ref) {
+  return AddUserAddressUsecase(addressRepo: ref.read(addressRepoImplProvider));
+});
