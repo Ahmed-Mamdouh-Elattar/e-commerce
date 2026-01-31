4bd86e6f748a27cc9cd2570e6a6e48df8c66d96f
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-01-31 11:32:05 +0200

    Implements address submission and refactors phone input
    Integrates the logic to save a user's address by collecting all necessary details.
    
    - Updates the `PhoneInputField` to use a `ValueNotifier` for better reactivity and ensures the complete phone number is captured, including a default country code.
    - Stores geolocation coordinates from the `gpsProvider` to include latitude and longitude with the address.
    - Refines the extraction of city, state, and street from GPS data.
    - Introduces `addUserAddressProvider` to manage the asynchronous process of adding an address.
    - Constructs a `UserAddressEntity` with all collected information (phone number, address components, and geolocation) upon form submission.
    - Provides UI feedback (loading, success, and error dialogs) during the address submission process.

--- lib/feature/address/presentation/widgets/phone_input_field.dart
+++ lib/feature/address/presentation/widgets/phone_input_field.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:phone_text_field/phone_text_field.dart';
 
 class PhoneInputField extends StatelessWidget {
-  const PhoneInputField({required this.phoneController, super.key});
-  final TextEditingController phoneController;
+  const PhoneInputField({required this.phoneValueNotifier, super.key});
+  final ValueNotifier<String> phoneValueNotifier;
 
   @override
   Widget build(BuildContext context) {
     return PhoneTextField(
       isRequired: true,
+      initialCountryCode: "EG",
       autovalidateMode: AutovalidateMode.onUserInteraction,
       decoration: const InputDecoration(hintText: "Enter your Phone Number"),
       onChanged: (phoneNumber) {
-        // Handle phone number changes
+        phoneValueNotifier.value = phoneNumber.completeNumber;
       },
     );
   }
--- lib/feature/address/presentation/widgets/take_user_address_page_body.dart
+++ lib/feature/address/presentation/widgets/take_user_address_page_body.dart
@@ -2,13 +2,16 @@ import 'package:e_commerce/core/helper/assets.gen.dart';
 import 'package:e_commerce/core/helper/constansts.dart';
 import 'package:e_commerce/core/helper/show_custom_dialogs.dart';
 import 'package:e_commerce/core/widgets/custom_eleveted_button.dart';
-import 'package:e_commerce/feature/address/presentation/providers/gps_provider.dart';
+import 'package:e_commerce/feature/address/domain/entities/user_address_entity.dart';
+import 'package:e_commerce/feature/address/presentation/providers/add_user_address_provider/add_user_address_provider.dart';
+import 'package:e_commerce/feature/address/presentation/providers/gps_provider/gps_provider.dart';
 import 'package:e_commerce/feature/address/presentation/widgets/building_and_floor_inputs_fields.dart';
 import 'package:e_commerce/feature/address/presentation/widgets/city_and_state_inputs_fields.dart';
 import 'package:e_commerce/feature/address/presentation/widgets/phone_input_field.dart';
 import 'package:e_commerce/feature/address/presentation/widgets/street_address_input_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -18,21 +21,22 @@ class TakeUserAddressPageBody extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final formKey = useMemoized(() => GlobalKey<FormState>());
-    final phoneController = useTextEditingController();
+    final phoneValueNotifier = useValueNotifier("");
     final streetAddressController = useTextEditingController();
     final cityController = useTextEditingController();
     final stateController = useTextEditingController();
     final buildingController = useTextEditingController();
     final floorController = useTextEditingController();
     final streetAddressFocusNode = useFocusNode();
+    final position = useRef<Position?>(null);
     ref.listen(gpsProvider, (previous, next) {
       next.when(
         data: (data) {
           context.pop();
-          cityController.text = data?.locality ?? "";
-          stateController.text = data?.administrativeArea ?? "";
-          String streetName = data?.street ?? "";
-
+          position.value = data?.position;
+          cityController.text = data?.place.locality ?? "";
+          stateController.text = data?.place.administrativeArea ?? "";
+          String streetName = data?.place.street ?? "";
           if (streetName.contains('+') ||
               RegExp(r'^[0-9]+$').hasMatch(streetName)) {
             streetName = "";
@@ -54,6 +58,29 @@ class TakeUserAddressPageBody extends HookConsumerWidget {
         },
       );
     });
+    ref.listen(addUserAddressProvider, (previous, next) {
+      next.when(
+        data: (data) {
+          context.pop();
+          showMessageDialog(
+            context,
+            message: "Address added successfully",
+            image: Assets.images.success.path,
+          );
+        },
+        loading: () {
+          showLoadingDialog(context);
+        },
+        error: (error, stackTrace) {
+          context.pop();
+          showMessageDialog(
+            context,
+            message: error.toString(),
+            image: Assets.images.error.path,
+          );
+        },
+      );
+    });
     return SingleChildScrollView(
       padding: const EdgeInsets.symmetric(horizontal: kPadding),
       child: Form(
@@ -61,7 +88,7 @@ class TakeUserAddressPageBody extends HookConsumerWidget {
         child: Column(
           children: [
             const SizedBox(height: 20),
-            PhoneInputField(phoneController: phoneController),
+            PhoneInputField(phoneValueNotifier: phoneValueNotifier),
             const SizedBox(height: 20),
             CityAndStateInputsFields(
               cityController: cityController,
@@ -89,7 +116,22 @@ class TakeUserAddressPageBody extends HookConsumerWidget {
             CustomElevetedButton(
               label: "Save Address",
               onPressed: () {
-                if (formKey.currentState!.validate()) {}
+                if (formKey.currentState!.validate()) {
+                  ref
+                      .read(addUserAddressProvider.notifier)
+                      .addAddress(
+                        UserAddressEntity(
+                          phoneNumber: phoneValueNotifier.value,
+                          city: cityController.text,
+                          state: stateController.text,
+                          streetAddress: streetAddressController.text,
+                          building: buildingController.text,
+                          floorNumber: floorController.text,
+                          latitude: position.value?.latitude,
+                          longitude: position.value?.longitude,
+                        ),
+                      );
+                }
               },
             ),
           ],
