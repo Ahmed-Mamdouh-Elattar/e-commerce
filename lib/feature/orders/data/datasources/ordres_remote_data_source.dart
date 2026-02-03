ce09380fe66b38c1f56af2b073e1490b9d8b5aac
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-02-03 19:27:32 +0200

    Fetch order products from database
    Adds a new method to retrieve the products associated with a specific order.
    
    This includes fetching product details like ID, title, images, and price alongside each order item.
    Error handling is included for user authentication and database exceptions.

--- lib/feature/orders/data/datasources/ordres_remote_data_source.dart
+++ lib/feature/orders/data/datasources/ordres_remote_data_source.dart
@@ -1,9 +1,13 @@
+import 'dart:developer';
+
 import 'package:e_commerce/core/error/failure.dart';
 import 'package:e_commerce/core/models/order_model.dart';
+import 'package:e_commerce/core/models/oreder_item_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class OrdresRemoteDataSource {
   Future<List<OrderModel>> getOrders();
+  Future<List<OrederItemModel>> getOrderProducts(String orderId);
 }
 
 class OrdresRemoteDataSourceImpl implements OrdresRemoteDataSource {
@@ -19,6 +23,34 @@ class OrdresRemoteDataSourceImpl implements OrdresRemoteDataSource {
           .select('*')
           .eq('user_id', userId);
       return response.map((e) => OrderModel.fromJson(e)).toList();
+    } on PostgrestException catch (e) {
+      throw Failure(message: e.message);
+    } catch (e) {
+      throw Failure(message: e.toString());
+    }
+  }
+
+  @override
+  Future<List<OrederItemModel>> getOrderProducts(String orderId) async {
+    try {
+      final userId = Supabase.instance.client.auth.currentUser?.id;
+      if (userId == null) {
+        throw Failure(message: "User not found");
+      }
+      final response = await Supabase.instance.client
+          .from('order_items')
+          .select('''
+      *,
+      products (
+        id,
+        title,
+        images,
+        price
+      )
+    ''')
+          .eq('order_id', orderId);
+      log(response.toString());
+      return response.map((e) => OrederItemModel.fromJson(e)).toList();
     } on PostgrestException catch (e) {
       throw Failure(message: e.message);
     } catch (e) {
