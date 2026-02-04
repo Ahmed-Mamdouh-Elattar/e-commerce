a6268bba7c60e1c196ffe90f13c2ae35d7e14444
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2026-02-04 13:14:07 +0200

    Initializes Firebase in the app
    Adds Firebase initialization to the application.
    
    This ensures that Firebase services are properly set up and available when the app starts.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:e_commerce/core/config/app_theme.dart';
 import 'package:e_commerce/core/helper/constansts.dart';
 import 'package:e_commerce/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:e_commerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -10,6 +12,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load(fileName: ".env");
   await Supabase.initialize(
     url: dotenv.env[subabaseUrl]!,
